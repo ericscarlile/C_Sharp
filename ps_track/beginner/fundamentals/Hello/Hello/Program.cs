b4f61c551cffe06bce534a0064a4625812b32fd0
Eric <ericscarlile@gmail.com>
2017-09-09 14:02:00 -0500

    Expand Hello World with inputs, if/else.

--- ps_track/beginner/fundamentals/Hello/Hello/Program.cs
+++ ps_track/beginner/fundamentals/Hello/Hello/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
@@ -10,6 +6,21 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursSlept = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, " + name);
+
+            if (hoursSlept >8)
+            {
+                Console.WriteLine("You are well rested!");
+            }
+            else
+            {
+                Console.WriteLine("You are not well rested!");
+            }
         }
     }
 }
